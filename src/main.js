5d21522a8c5b1897211c1c23b0748d7c1fdd96e9
autumnfish <517729329@qq.com>
2019-04-30 10:39:10 +0800

    头部尾部整合

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入全局样式
+// 如果只是导入 不使用 可以省略 xxx from
+// import baseCss from './assets/statics/site/css/style.css'
+import './assets/statics/site/css/style.css'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
